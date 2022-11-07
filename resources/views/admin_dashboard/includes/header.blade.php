d1cd7111cb2ff42a89ec724275474b1335eb52fd
haider10954 <chhaider10954@gmail.com>
2022-11-07 18:20:32 +0500

    Status Symbol added in header

--- resources/views/admin_dashboard/includes/header.blade.php
+++ resources/views/admin_dashboard/includes/header.blade.php
@@ -1,9 +1,22 @@
+<style>
+    .status {
+        height: 8px;
+        width: 8px;
+        background-color: #71DD37;
+        position: absolute;
+        bottom: 25%;
+        right: 27%;
+        border-radius: 50%;
+        border: none;
+    }
+</style>
+
 <header id="page-topbar">
     <div class="navbar-header" id="header">
         <div class="d-flex">
             <!-- LOGO -->
             <div class="navbar-brand-box">
-                <a  class="logo logo-light">
+                <a class="logo logo-light">
                     <span class="logo-sm">
                         <img src="{{ asset('assets/images/index.png') }}" alt="" height="22">
                     </span>
@@ -104,9 +117,9 @@
             </div>
 
             <div class="dropdown d-inline-block">
-                <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <button type="button" class="btn header-item waves-effect position-relative" id="page-header-user-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <img class="rounded-circle header-profile-user" src="{{ asset('assets/images/users/avatar-1.jpg') }}" alt="Header Avatar">
-                    <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
+                    <div class="status"></div>
                 </button>
                 <div class="dropdown-menu dropdown-menu-end">
                     <a class="dropdown-item text-danger" href="{{ route('admin_auth_logout') }}"><i class="bx bx-power-off font-size-16 align-middle me-1 text-danger"></i> <span key="t-logout">Logout</span></a>
