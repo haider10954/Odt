c552b29ba064b06ae904c3d2b2733dfee1b4c17a
Iamqasimmansoor <qasimmansoor683@gmail.com>
2022-11-10 13:45:58 +0500

    Book Detail Images Slideshow

--- resources/views/web/book_detail.blade.php
+++ resources/views/web/book_detail.blade.php
@@ -4,13 +4,23 @@
 
 @section('page_title',__('Book Detail'))
 
+@section('custom-style')
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fancyapps/ui/dist/fancybox.css"/>
+@endsection
+
 @section('content')
 <p class="font-weight-600">{{ $book->book_title }}</p>
 <div class="book-pages">
     @foreach ($book->book_pages as $item)
         <div class="book-page">
-            <img src="{{ asset('storage/books/pages/'.$item) }}" class="book-page-img">
+            <a href="{{ asset('storage/books/pages/'.$item) }}" data-fancybox="gallery">
+                <img src="{{ asset('storage/books/pages/'.$item) }}" class="book-page-img">
+            </a>
         </div>
     @endforeach
 </div>
+@endsection
+
+@section('custom-script')
+<script src="https://cdn.jsdelivr.net/npm/@fancyapps/ui@4.0/dist/fancybox.umd.js"></script>
 @endsection
