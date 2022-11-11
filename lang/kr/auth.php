f5b1160092d447881cc09adea3cbe75ea43f78e6
Iamqasimmansoor <qasimmansoor683@gmail.com>
2022-11-11 17:01:16 +0500

    Korean of reset passwords

---
+++ lang/kr/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '이 자격 증명은 우리의 기록과 일치하지 않습니다.',
+    'password' => '이 상품은 우리의 이성과.',
+    'throttle' => '너무 많은 로그인 시도. 에서 다시 시도하십시오 :seconds 초.',
+
+];
