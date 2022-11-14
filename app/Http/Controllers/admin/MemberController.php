16e16d317e1fe6057d9d33048b86505534b60226
haider10954 <chhaider10954@gmail.com>
2022-11-14 13:38:59 +0500

    Member Toast Message changed to Korean

--- app/Http/Controllers/admin/MemberController.php
+++ app/Http/Controllers/admin/MemberController.php
@@ -11,16 +11,16 @@ class MemberController extends Controller
     public function member_listing()
     {
         $user = User::paginate(5);
-        return view('admin_dashboard.member_management.member_management' , compact('user'));
+        return view('admin_dashboard.member_management.member_management', compact('user'));
     }
     public function delete_user(Request $request)
     {
 
         $user = User::where('id', $request['id'])->delete();
         if ($user) {
-            return redirect()->back()->with('removed', 'User has been removed successfully ');
+            return redirect()->back()->with('removed', __('translation.User has been removed successfully '));
         } else {
-            return redirect()->back()->with('error', 'Something went wrong, please try again');
+            return redirect()->back()->with('error', __('translation.Something went wrong Please try again.'));
         }
     }
 }
