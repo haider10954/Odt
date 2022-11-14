da9d4145a48bb9e4498ff85b68f0105d9b56bc4b
haider10954 <chhaider10954@gmail.com>
2022-11-14 13:40:26 +0500

    Admin Login Toast changed to korean

--- app/Http/Controllers/admin/AuthController.php
+++ app/Http/Controllers/admin/AuthController.php
@@ -8,19 +8,21 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    public function login(Request $request){
+    public function login(Request $request)
+    {
         $request->validate([
             'email' => 'required|email',
             'password' => 'required'
         ]);
-        if(Auth::guard('admin')->attempt(['email' => $request->email , 'password' => $request->password ])){
+        if (Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password])) {
             return redirect()->route('ticket-management');
-        }else{
-            return redirect()->back()->with('message','Email or password is invalid');
+        } else {
+            return redirect()->back()->with('message', __('translation.Email or password is invalid'));
         }
     }
 
-    public function logout(){
+    public function logout()
+    {
         Auth::guard('admin')->logout();
         return redirect()->route('admin-login');
     }
