32600c65e5441b1979fc6351443cac811876bed3
haider10954 <chhaider10954@gmail.com>
2022-11-16 11:10:46 +0500

    Error Message Set on Login

--- resources/views/admin_dashboard/login/login.blade.php
+++ resources/views/admin_dashboard/login/login.blade.php
@@ -74,10 +74,10 @@
                                         <div class="input-group auth-pass-inputgroup">
                                             <input type="password" class="form-control" name="password" placeholder="{{ __('translation.Enter password')}}" aria-label="Password" aria-describedby="password-addon">
                                             <button class="btn btn-light " type="button" id="password-addon"><i class="mdi mdi-eye-outline"></i></button>
-                                            @error('password')
-                                            <p class="text-danger">{{ $message }}</p>
-                                            @enderror
                                         </div>
+                                        @error('password')
+                                        <p class="text-danger">{{ $message }}</p>
+                                        @enderror
                                     </div>
 
                                     <div class="mt-3 d-grid">
